08401dfa96a87bc4b0f627ae68f1ac19b4827876
Catalina Popa <catalina.popa@fundmore.ai>
2025-05-03 11:15:53 +0300

    v17

--- planet-explorer-frontend/src/app/app.module.ts
+++ planet-explorer-frontend/src/app/app.module.ts
@@ -8,16 +8,7 @@ import { PlanetDetailComponent } from './features/planet/planet-detail/planet-de
 import { HttpClientModule } from '@angular/common/http';
 import { PlanetDetailFormComponent } from './features/planet/planet-detail-form/planet-detail-form.component';
 import { FormsModule } from '@angular/forms';
-import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
-import { MatLegacyChipsModule as MatChipsModule } from '@angular/material/legacy-chips';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
-import { MatLegacyOptionModule as MatOptionModule } from '@angular/material/legacy-core';
-import { MatLegacySelectModule as MatSelectModule } from '@angular/material/legacy-select';
 
 @NgModule({
   declarations: [
@@ -32,15 +23,15 @@ import { MatLegacySelectModule as MatSelectModule } from '@angular/material/lega
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    MatToolbarModule,
-    MatCardModule,
-    MatListModule,
-    MatIconModule,
-    MatDividerModule,
-    MatChipsModule,
-    MatFormFieldModule,
-    MatOptionModule,
-    MatSelectModule,
+    // MatToolbarModule,
+    // MatCardModule,
+    // MatListModule,
+    // MatIconModule,
+    // MatDividerModule,
+    // MatChipsModule,
+    // MatFormFieldModule,
+    // MatOptionModule,
+    // MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
