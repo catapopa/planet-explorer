c79725fd2b82924af96e217a0dc15aa47934b8e7
Catalina Popa <catalina.popa@fundmore.ai>
2025-05-03 11:19:56 +0300

    mat v18

--- planet-explorer-frontend/src/app/app.module.ts
+++ planet-explorer-frontend/src/app/app.module.ts
@@ -5,19 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlanetListComponent } from './features/planet/planet-list/planet-list.component';
 import { PlanetDetailComponent } from './features/planet/planet-detail/planet-detail.component';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { PlanetDetailFormComponent } from './features/planet/planet-detail-form/planet-detail-form.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-@NgModule({ declarations: [
-        AppComponent,
-        PlanetListComponent,
-        PlanetDetailComponent,
-        PlanetDetailFormComponent,
-    ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        FormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+@NgModule({
+  declarations: [
+    AppComponent,
+    PlanetListComponent,
+    PlanetDetailComponent,
+    PlanetDetailFormComponent,
+  ],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+  ],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
+})
 export class AppModule {}
